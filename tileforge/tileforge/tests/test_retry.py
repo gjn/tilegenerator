06b31cc9f5005651599f97f2a580848389be9753
ltclm <ltclm@bd729b38-e83c-4c31-ba7f-6b5d0bf862be>
2011-03-18 08:01:23 +0000

    tileforge.utils.retry: fix unit tests
    git-svn-id: https://svn.bgdi.admin.ch/tilegenerator/trunk@56 bd729b38-e83c-4c31-ba7f-6b5d0bf862be

--- tileforge/tileforge/tests/test_retry.py
+++ tileforge/tileforge/tests/test_retry.py
@@ -1,16 +1,19 @@
 import unittest
 from StringIO import StringIO
 from tileforge.utils.retry import dump, load
+from tileforge.tests import tilecache_service
 
 class TestRetry(unittest.TestCase):
     def test_dump_load(self):
-        in_layername = "this_is_the_layername"
+        service = tilecache_service()
+        raster = service.layers.get("valid-raster")
+        
         in_tiles = zip(range(10), range(10,20), range(20, 30))
         f = StringIO()
-        dump(in_layername, in_tiles, f)
+        dump(raster, in_tiles, f)
         f.seek(0)
         out_layername, out_tiles = load(f)
-        
-        assert out_layername == in_layername
-        assert out_tiles == in_tiles
         f.close()
+        
+        assert out_layername == raster.name
+        assert out_tiles == in_tiles
